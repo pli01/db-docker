049719254c692959de41615cecf7551364d1cc3a
pli01 <pli01@users.noreply.github.com>
2021-10-05 16:18:42 +0200

    Add posgresql + pgadmin

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # db-docker
+
+Simple db docker stack
+* postgres: postgresql + pgadmin
+* mysql: mysql + phpmyadmin
+
+
+## Launch
+
+```
+make up-postgres
+make down-postgres
+```
--- ci/docker-deploy.sh
+++ ci/docker-deploy.sh
@@ -10,8 +10,8 @@ export DOCKERHUB_TOKEN="${DOCKERHUB_TOKEN:-}"
 export DOCKER_REGISTRY_USERNAME="${DOCKER_REGISTRY_USERNAME:?DOCKER_REGISTRY_USERNAME not set}"
 export DOCKER_REGISTRY_TOKEN="${DOCKER_REGISTRY_TOKEN:?DOCKER_REGISTRY_TOKEN not set}"
 
-export APP_NAME="${APP_NAME:-dataiku-dss-docker}"
-export APP_BRANCH="${APP_BRANCH:-master}"
+export APP_NAME="${APP_NAME:-db-docker}"
+export APP_BRANCH="${APP_BRANCH:-main}"
 export APP_URL="https://github.com/pli01/${APP_NAME}/archive/refs/heads/${APP_BRANCH}.tar.gz"
 
 # if authenticated repo
@@ -32,7 +32,6 @@ eval curl -kL -s $curl_args ${APP_URL} | \
 ( cd ${APP_NAME}
   if [ -n "$DOCKERHUB_TOKEN" -a -n "$DOCKERHUB_LOGIN" ] ;then  echo $DOCKERHUB_TOKEN | docker login --username $DOCKERHUB_LOGIN --password-stdin ; fi
 
-  make pull-image
   make up$app_role
   if [ -n "$DOCKERHUB_TOKEN" -a -n "$DOCKERHUB_LOGIN" ] ; then docker logout ; fi
 )
